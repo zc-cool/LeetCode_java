12f07dbe3923fa183d653f04be821e4176f4b883
zc <2809779139@qq.com>
2021-03-10 23:10:15 +0800

    leetcode27

--- src/easy/array/Hello.java
+++ src/easy/array/Hello.java
@@ -8,4 +8,28 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("===");
     }
+
+    public int removeElement(int[] nums, int val) {
+        if(nums.length == 0) return 0;
+        int prep = 0;
+        int last = nums.length - 1;
+        while(prep<last){
+            if(nums[prep] != val){
+                prep++;
+                continue;
+            }else {
+                if(nums[last] == val){
+                    last--;
+                    continue;
+                }else{
+                    int tem = nums[prep];
+                    nums[prep] = nums[last];
+                    nums[last] = tem;
+                    prep++;
+                    last--;
+                }
+            }
+        }
+        return last+1;
+    }
 }
